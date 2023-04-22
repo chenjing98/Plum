34a06d180149dd50e3acf8ce4eeffed378d6c5af
chenjing98 <j_chen98@outlook.com>
2023-04-22 17:22:12 +0000

    feat(eval): change eval script to p2p test

--- evaluation/batch-run-test.sh
+++ evaluation/batch-run-test.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-export CORE_COUNT=20
+export CORE_COUNT=60
 
-declare -a seeds=(1000000 82734 12946 129 89085321 5 120000 8765432 19 777)
-declare -a nclients=(3 4 5 10)
-declare simTime=(1200)
+declare -a seeds=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 12946 129 777)
+declare -a nclients=(3 4 5 6 7 8 9 10 20)
+declare simTime=(1800)
 declare -a policies=(0 1)
+declare -a ulprops=(0.1)
 
-export FPS=60
-export BITRATE=16  # Mbps
-mtu=1406
-export DATA_PER_FRAME=$(awk 'BEGIN{printf "%d\n", '$BITRATE'*1e6/8/'$FPS'/'$mtu+1'}')
+# export FPS=60
+# export BITRATE=10  # Mbps
+# mtu=1406
+# export DATA_PER_FRAME=$(awk 'BEGIN{printf "%d\n", '$BITRATE'*1e6/8/'$FPS'/'$mtu+1'}')
 
 export NS3_THROUGHPUT_REGEX="\[VcaClient\]\[Result\] Throughput= ([0-9\.e\-]+)"
 
@@ -24,11 +25,12 @@ run_ns3() {
     seed=$2
     nclient=$3
     simt=$4
-    filename=${RESULT_DIR}/result
+    ulprop=$5
+    filename=${RESULT_DIR}/result_p2p_${ulprop}
     output_file=${filename}.txt
     output_file_clean=${filename}.csv
     echo At policy: $policy, nclient: $nclient, seed: $seed, output_file: $filename.txt/csv...
-    ns3_output=$(NS_GLOBAL_VALUE="RngRun=$seed" ./ns3 run "scratch/test+wifi+app --mode=sfu --logLevel=0 --simTime=${simt} --policy=${policy} --nClient=${nclient}" 2>&1)
+    ns3_output=$(NS_GLOBAL_VALUE="RngRun=$seed" ./ns3 run "scratch/test_half_duplex --mode=sfu --logLevel=0 --simTime=${simt} --policy=0 --nClient=${nclient} --varyBw --traceMode=${policy} --ulProp=${ulprop} --seed=${seed}" 2>&1)
     avg_thp=$(python3 /home/chenj/UplinkCoordination/evaluation/log-process.py -l "${ns3_output}" -a)
     tail_thp=$(python3 /home/chenj/UplinkCoordination/evaluation/log-process.py -l "${ns3_output}" -t)
     # miss_rate=$(awk 'BEGIN{printf "%.010f\n",'$missed_frame_cnt'/'$total_frame_cnt'}')
@@ -49,6 +51,11 @@ cd $NS3_DIR
 
 export -f run_ns3
 
-echo "policy, nclient, seed, avg_thp, tail_thp" > ${RESULT_DIR}/result.csv
 
-parallel -j${CORE_COUNT} run_ns3 ::: ${policies[@]} ::: ${seeds[@]} ::: ${nclients[@]} ::: ${simTime}
+for ulprop in ${ulprops[@]}; do
+    echo "policy, nclient, seed, avg_thp, tail_thp" > ${RESULT_DIR}/result_p2p_${ulprop}.csv
+done
+
+
+
+parallel -j${CORE_COUNT} run_ns3 ::: ${policies[@]} ::: ${seeds[@]} ::: ${nclients[@]} ::: ${simTime} ::: ${ulprops[@]}
